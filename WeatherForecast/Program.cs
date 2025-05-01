91ed921c2bbe172165a5b29b636bf16b97d0dcef
Mohammed Umer M B <83535174+umarshk-mb@users.noreply.github.com>
2025-05-01 22:00:59 +0530

    Update Program.cs

--- WeatherForecast/Program.cs
+++ WeatherForecast/Program.cs
@@ -6,6 +6,9 @@ using Weather.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://*:{port}");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
